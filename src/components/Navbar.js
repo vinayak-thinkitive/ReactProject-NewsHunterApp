cf0cf19a15cb2c255da130e6d1f8650dbc25fd4d
vinayak-thinkitive <vinayak.mane@thinkitive.com>
2023-10-05 13:38:19 +0530

    used useLocation hook in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component, useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
-export default class
-    extends Component {
-    render() {
+const Navbar = (props) => {
+
+    let location = useLocation();
+    useEffect(() => {
+      console.log(location.pathname)
+    }, [location])
+    
+
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -15,28 +20,28 @@ export default class
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                                    <Link className={`nav-link ${location.pathname === "/"?"active":""}`} aria-current="page" to="/">Home</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/about">About Us</Link>
+                                    <Link className={`nav-link ${location.pathname === "/about"?"active":""}`} to="/about">About Us</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/business">Business</Link>
+                                    <Link className={`nav-link ${location.pathname === "/business"?"active":""}`} to="/business">Business</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/entertainment">Entertainment</Link>
+                                    <Link className={`nav-link ${location.pathname === "/entertainment"?"active":""}`} to="/entertainment">Entertainment</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/health">Health</Link>
+                                    <Link className={`nav-link ${location.pathname === "/health"?"active":""}`} to="/health">Health</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/science">Science</Link>
+                                    <Link className={`nav-link ${location.pathname === "/science"?"active":""}`} to="/science">Science</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/sports">Sports</Link>
+                                    <Link className={`nav-link ${location.pathname === "/sports"?"active":""}`} to="/sports">Sports</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/technology">Technology</Link>
+                                    <Link className={`nav-link ${location.pathname === "/technology"?"active":""}`} to="/technology">Technology</Link>
                                 </li>
                             </ul>
                         </div>
@@ -44,5 +49,7 @@ export default class
                 </nav>
             </div>
         )
-    }
 }
+
+
+export default Navbar
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -49,7 +49,7 @@ export class NewsItem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <p className="card-text"><small class="text-body-secondary">By {author?author:"UNKOWN SOURCE"} on {new Date(date).toGMTString()}</small></p>
+            <p className="card-text"><small className="text-body-secondary">By {author?author:"UNKOWN SOURCE"} on {new Date(date).toGMTString()}</small></p>
             <a href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More...</a>
           </div>
         </div>
